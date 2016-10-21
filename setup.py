fdddd2fd3eb8450034092a914d219ac01597ade6
Luca Marturana <lucamarturana@gmail.com>
2016-10-21 18:30:30 +0200

    pip release

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='sysdig-tracers',
-      version='0.1.0',
+      version='0.1.1',
       description='Python library to emit Sysdig tracers',
       url='http://github.com/draios/tracers-py',
       author='sysdig Inc.',
       author_email='info@sysdig.com',
       license='MIT',
-      py_modules=['sysdig-tracers'],
+      py_modules=['sysdig_tracers'],
       zip_safe=False)
